b7218103abac0617b96c22a758a9a6ed7b06c7e9
Nakamura, Yuuichi <Yuuichi.A.Nakamura@sony.com>
2020-06-16 12:16:17 +0900

    Device registration for Arm and RISC-V

--- arch/risc-v/src/common/riscv_initialize.c
+++ arch/risc-v/src/common/riscv_initialize.c
@@ -45,6 +45,7 @@
 #include <nuttx/arch.h>
 #include <nuttx/board.h>
 #include <nuttx/sched_note.h>
+#include <nuttx/sched_tracer.h>
 #include <nuttx/serial/pty.h>
 #include <nuttx/syslog/syslog_console.h>
 #include <nuttx/drivers/drivers.h>
@@ -148,6 +149,11 @@ void up_initialize(void)
 #if defined(CONFIG_SCHED_INSTRUMENTATION_BUFFER) && \
     defined(CONFIG_DRIVER_NOTE)
   note_register();    /* Non-standard /dev/note */
+#endif
+
+#if defined(CONFIG_SCHED_INSTRUMENTATION_TRACER) && \
+    defined(CONFIG_DRIVER_TRACER)
+  tracer_register();    /* Non-standard /dev/tracer */
 #endif
 
   /* Initialize the serial device driver */
